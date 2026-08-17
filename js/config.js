fdb114c33a654a083bbf5d02284e47929b588406
sesaonara <sesaonara@users.noreply.github.com>
2026-08-17 19:06:55 +0700

    fix: ANYONE_ANONYMOUS deploy @10 + CORS fetch - API working!

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // Google Apps Script Web App URL
   // แก้ไข URL นี้หลังจาก Deploy Apps Script เป็น Web App
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwkFgzWUHySvFHkHSyfpXYc0d6jnIgMxsd0DUAAkCAy2OUDSO-gDbfSqSrutBFxcLoLeg/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxegsx81c0ADCZy3d7doQpgYaaM4BpZTFuJo1nxGvih61WPI-fbnVRz5E_ZaAls5n3v/exec',
 
   // Google Sheets ID
   SPREADSHEET_ID: '1KHJVpe1w4gRPAckL_gbs0WrGEjnDJylwny0od7jvkQM',
